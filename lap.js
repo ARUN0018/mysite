5c095547aa56ef1799ec1dee2f2b5ddd7158e71e
swdev <swdev@sanctumnetworks.com>
2023-08-23 17:29:07 +0530

    http method

--- lap.js
+++ lap.js
@@ -18,19 +18,20 @@ const bodyReader = (req) => {
 //
 // })
 
-const books = [];
-
 const requestListener = function (req, res) {
   if (req.method === "POST") {
     if (req.url === "/books") {
       bodyReader(req).then(function (body) {
         const book = JSON.parse(body);
+
         fs.readFile("lap.txt", { encoding: "utf8" }, function (err, data) {
           if (err) {
             console.log("Err");
+            res.writeHead(500);
+            res.end();
           } else {
             const books = JSON.parse(data);
-            books.push(a, book);
+            books.push(book);
             fs.writeFile("lap.txt", JSON.stringify(books), (err) => {
               if (err) throw err;
             });
@@ -40,14 +41,61 @@ const requestListener = function (req, res) {
           }
         });
       });
-    } else {
-      res.writeHead(400, { "Content-type": "application/json" });
-      res.write(JSON.stringify("not found"));
-      res.end();
     }
+  } else if (req.method == "GET" && req.url == "/books") {
+    fs.readFile("lap.txt", { encoding: "utf8" }, function (err, data) {
+      if (err) {
+        console.log("error");
+        res.writeHead(500);
+        res.end();
+      } else {
+        res.writeHead(200);
+        res.write(data);
+        res.end();
+      }
+    });
+  } else if (req.method == "DELETE" && req.url == "/books") {
+    bodyReader(req).then(function (body) {
+      const book = JSON.parse(body).name;
+
+      fs.readFile("lap.txt", { encoding: "utf8" }, function (err, data) {
+        if (err) {
+          console.log("error");
+          res.writeHead(500);
+          res.end();
+        } else {
+          const books = JSON.parse(data);
+          const index = books.findIndex(function (author) {
+            return author.name === book;
+          });
+
+          if (index !== -1) {
+            books.splice(index, 1);
+            fs.writeFile("lap.txt", JSON.stringify(books), function (err) {
+              if (err) {
+                console.log("error");
+                res.writeHead(500);
+                res.end();
+              } else {
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.write(JSON.stringify({ element: "deleted" }));
+                res.end();
+              }
+            });
+          } else {
+            console.log("error");
+            res.writeHead(500);
+            res.end();
+          }
+        }
+      });
+    });
+  } else {
+    res.writeHead(400, { "Content-type": "application/json" });
+    res.write(JSON.stringify("not found"));
+    res.end();
   }
 };
-
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
   console.log(`Server on running http://${host}${port}`);
--- method.js
+++ method.js
@@ -20,20 +20,29 @@ const bodyReader = (req) => {
 
 const players = [];
 
+fs.writeFile("");
+
 const requestListener = function (req, res) {
   if (req.method == "POST") {
     console.log(req.url);
     if (req.url == "/player") {
       bodyReader(req).then(function (body) {
         const player = JSON.parse(body);
-        players.push(player);
-        fs.appendFile("file.txt", JSON.stringify({ player }), (err) => {
-          if (err) throw err;
+
+        fs.readFile("file.txt", { encoding: "utf8" }, function (err, data) {
+          if (err) {
+            console.log("Err");
+          } else {
+            const players = JSON.parse(data);
+            players.push(player);
+            fs.writeFile("file.txt", JSON.stringify(players), (err) => {
+              if (err) throw err;
+            });
+            res.writeHead(200, { "Content-type": "application/json" });
+            res.write(body);
+            res.end();
+          }
         });
-        res.writeHeader(200, { "Content-Type": "application/json" });
-        res.write(body);
-        res.end();
-        return;
       });
     } else {
       res.writeHeader(404);
@@ -53,33 +62,31 @@ const requestListener = function (req, res) {
         if (err) throw err;
         else {
           console.log("File read");
-          const players = function () {
-            players = players(data);
-          };
+          const players = data;
         }
       });
-      bodyReader(req).then(function (body) {
-        const playerToDelete = JSON.stringify(body).name;
-        const index = players.findIndex(function (players) {
-          player.name === playerToDelete;
-        });
-        if (index !== -1) {
-          players.splice(index, 1);
-
-          fs.writeFile("file.txt", players, { encoding: "utf8" }, (err) => {
-            if (err) throw err;
-            console.log("player deleted and file updated!");
-          });
-        }
-        res.writeHeader(200);
-        res.write(JSON.stringify({ element: " deleted" }));
-        res.end();
-        return;
-      });
-    } else {
-      res.writeHeader(404);
-      res.end(JSON.stringify({ error: "Not found" }));
     }
+    bodyReader(req).then(function (body) {
+      const playerToDelete = JSON.stringify(body).name;
+      const index = players.findIndex(function (players) {
+        player.name === playerToDelete;
+      });
+      if (index !== -1) {
+        players.splice(index, 1);
+
+        fs.writeFile("file.txt", players, { encoding: "utf8" }, (err) => {
+          if (err) throw err;
+          console.log("player deleted and file updated!");
+        });
+      }
+      res.writeHeader(200);
+      res.write(JSON.stringify({ element: " deleted" }));
+      res.end();
+      return;
+    });
+  } else {
+    res.writeHeader(404);
+    res.end(JSON.stringify({ error: "Not found" }));
   }
 };
 const server = http.createServer(requestListener);
